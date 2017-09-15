ad46cbddc97e0d5128eceac0cf847a6551c9360c
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2017-09-15 10:05:46 +0530

    forced commit

--- evaluator/forms.py
+++ evaluator/forms.py
@@ -2,3 +2,6 @@ from django import forms
 
 class AnswerForm(forms.Form):
 	answer = forms.CharField(label="Your answer: ", required=True, widget=forms.Textarea)
+
+class AnswerFormOCR(forms.Form):
+	answer = forms.ImageField()
--- evaluator/views.py
+++ evaluator/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 import requests
 import json
 from .models import Question, Answer
-from .forms import AnswerForm
+from .forms import AnswerForm, AnswerFormOCR
 from django.forms.formsets import formset_factory
 
 
@@ -33,12 +33,12 @@ class AnswerView(View):
 		                	{'formset': AnswerFormSet(),
 		                	'questions': questions})
 
-	def post(self, request, *args, **kwargs):
+	def post(self, request,  *args, **kwargs):
+		Answer.objects.all().delete()
 		AnswerFormSet = formset_factory(AnswerForm)
 		formset = AnswerFormSet(request.POST)
 		questions = Question.objects.all()
 		for form in formset:
-			print(form)
 			formid = int(form.prefix[5:])
 			question = questions[formid]
 			text1 = question.answer
@@ -58,3 +58,36 @@ class AnswerView(View):
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
 		return render(request, "home.html", {})
+
+class AnswerViewOCR(View):
+
+	def get(self, request, *args, **kwargs):
+		Answer.objects.all().delete()
+		questions = Question.objects.all()
+		AnswerFormSet = formset_factory(AnswerFormOCR, extra=questions.count())
+		return render(request, 'answer_ocr.html',
+		                	{'formset': AnswerFormSet(),
+		                	'questions': questions})
+
+	def post(self, request, *args, **kwargs):
+		print(request.FILES)
+		Answer.objects.all().delete()
+		AnswerFormSet = formset_factory(AnswerFormOCR)
+		formset = AnswerFormSet(request.POST)
+		questions = Question.objects.all()
+		for form in formset:
+			formid = int(form.prefix[5:])
+			question = questions[formid]
+			text1 = question.answer
+			#text2 = form.data[form.prefix+"-answer"]
+			# token = "27cfd17d8c6a46758b2559c2e6df3388"
+			# marks = question.marks	#set by teacher
+
+			# url = "https://api.dandelion.eu/datatxt/sim/v1/?text1=%s&text2=%s&token=%s" %(text1.replace(" ", "%20"), text2.replace(" ", "%20"), token)
+
+			# r = requests.get(url).json()
+			# if "error" not in r:
+			# 	Answer.objects.create(question=question, your_response=text2 ,marks_awarded=round(0.5 * round(r["similarity"] * marks / 0.5) , 1))
+			# else:
+			# 	Answer.objects.create(question=question, marks_awarded=0, your_response="Unanswered." , unanswered = True)
+		return HttpResponseRedirect("/evaluator/evaluate/")
